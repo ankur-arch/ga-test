7da9cece0243c1c8632883232e2c9b88b3092579
Ankur Datta <datta@prisma.io>
2023-10-02 01:45:57 +0600

    feat: disable caching

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,9 @@
 import prisma from "@/utils/prisma";
 
+// disabling caching
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
+
 export async function GET() {
   const d = await fetch("https://api.quotable.io/random", {
     cache: "no-cache",
