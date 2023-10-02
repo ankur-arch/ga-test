f7bb627360727c107ff1584f07246ae3407ee410
Ankur Datta <datta@prisma.io>
2023-10-03 05:28:48 +0600

    feat: add accelerate connectin pooling

--- load-test.js
+++ load-test.js
@@ -1,12 +1,13 @@
 import http from "k6/http";
 import { sleep } from "k6";
+import { randomIntBetween } from "https://jslib.k6.io/k6-utils/1.2.0/index.js";
 
 export const options = {
-  vus: 300,
-  duration: "5s",
+  vus: 80,
+  duration: "10s",
 };
 
 export default function () {
-  http.get("https://ga-test-two.vercel.app/api/acc");
-  sleep(0.1);
+  http.get("https://nextjs-accelerate-api.vercel.app/api");
+  sleep(randomIntBetween(1, 2));
 }
